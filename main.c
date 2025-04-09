32260b63fdf3e5d0849034e876113a64dbdcb7d0
Hannes <hannes10042004@icloud.com>
2025-04-09 15:59:06 +0200

    Takes user input now

--- main.c
+++ main.c
@@ -1,9 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define RED "\033[0;31m"
+#define RESET "\e[0m"
+
+void check_buffer(char * buffer);
+char *read_command();
 
 void loop() {
     int status = 1;
     while (status) {
-        printf("<(^^)>");
+        printf("<(^^)> ");
+        read_command();
+    }
+}
+
+char *read_command() {
+    const int buffer_steps = 512;
+    int buffsize = buffer_steps;
+    char *buffer = malloc(sizeof(char) * buffsize);
+
+    //check if buffer has been properly allocated
+    check_buffer(buffer);
+
+    int buf_pos = 0;
+    int c = getchar();
+
+    while(c != EOF && c != '\n') {
+        buffer[buf_pos] = c;
+        buf_pos++;
+
+        if(buf_pos >= buffsize) {
+            buffsize += buffer_steps;
+            buffer = realloc(buffer, buffer_steps);
+            check_buffer(buffer);
+        }
+
+        c = getchar();
+    }
+
+    buffer[buf_pos] = '\0';
+    return buffer;
+}
+
+void check_buffer(char *buffer) {
+    if(!buffer) {
+        fprintf(stderr, "%s<(^^)>: Allocation did not succeed%s", RED, RESET);
+        exit(EXIT_FAILURE);
     }
 }
 
