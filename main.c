a05bb5ca2e123d9807afd011925e4c6c7fb63d31
Hannes <hannes10042004@icloud.com>
2025-04-12 19:58:41 +0200

    Added tokenisation to read inputs

--- main.c
+++ main.c
@@ -1,18 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define RED "\033[0;31m"
 #define RESET "\e[0m"
 
-void check_buffer(char * buffer);
+#define TK_BUFF_SIZE 64
+#define TOKEN_DELIMITER " \t\r\n"
+void check_buffer(char *buffer);
+void check_tokens(char **buffer);
 char *read_command();
-
+char **tokenize(char *buffer);
 void loop() {
     int status = 1;
     while (status) {
         printf("<(^^)> ");
-        read_command();
+        char *buffer = read_command();
+        tokenize(buffer);
     }
+}
+
+char **tokenize(char *buffer) {
+    const int buffsteps = 64;
+    int buffsize = buffsteps, buf_pos = 0;
+
+    char **tokens = malloc(TK_BUFF_SIZE * sizeof(char *));
+
+    check_tokens(tokens);
+
+    char *token = strtok(buffer, TOKEN_DELIMITER);
+
+    while(NULL != token) {
+        tokens[buf_pos] = token;
+        buf_pos++;
+
+        if(buf_pos >= buffsize) {
+            buffsize += buffsteps;
+            tokens = realloc(tokens, buffsize);
+
+            check_tokens(tokens);
+        }
+
+
+        token = strtok(NULL, TOKEN_DELIMITER);
+    }
+
+    return tokens;
 }
 
 char *read_command() {
@@ -20,7 +53,6 @@ char *read_command() {
     int buffsize = buffer_steps;
     char *buffer = malloc(sizeof(char) * buffsize);
 
-    //check if buffer has been properly allocated
     check_buffer(buffer);
 
     int buf_pos = 0;
@@ -45,9 +77,17 @@ char *read_command() {
 
 void check_buffer(char *buffer) {
     if(!buffer) {
-        fprintf(stderr, "%s<(^^)>: Allocation did not succeed%s", RED, RESET);
+        fprintf(stderr, "%s<(^^)>: Allocation did not succeed in buffer creation%s\n", RED, RESET);
         exit(EXIT_FAILURE);
     }
+}
+
+void check_tokens(char **buffer) {
+    if(!buffer) {
+        fprintf(stderr, "%s<(^^)>: Allocation did not succeed token creation%s\n", RED, RESET);
+        exit(EXIT_FAILURE);
+    }
+
 }
 
 int main() {
